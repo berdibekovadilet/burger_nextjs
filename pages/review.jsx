a5a899a086c61cc1fa1f91cbfadca0ea26b011b0
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-05 00:32:05 +0600

    added reviews

--- pages/review.jsx
+++ pages/review.jsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Review.module.css";
 
 const ReviewsPage = ({ reviews }) => {
   return (
@@ -7,34 +9,28 @@ const ReviewsPage = ({ reviews }) => {
         <title>Отзывы наших клиентов</title>
         <meta name="title" content="Все отзывы" />
       </Head>
-      <div>
-        <h1>Отзывы клиентов</h1>
-        <div className="reviews">
-          {!!reviews.length &&
-            reviews.slice(0, 20).map((res) => {
-              return (
-                <div key={res.id} className="review">
-                  {res.id}
-                  {` `}
-                  {`${res.body.slice(0, 90)}...`}
-                </div>
-              );
-            })}
-        </div>
+      <h1>Отзывы наших клиентов</h1>
+      <div className={styles.container}>
+        <Image src="/puff.png" alt="Avatar" width={100} height={100} />
+        <p>
+          <span>Миссис Пафф</span> Учитель вождения
+        </p>
+        <p>
+          Великопленые бургеры. Захожу кушать 3 раза в день, очень довольна.
+        </p>
+      </div>
+
+      <div className={styles.container}>
+        <Image src="/plankton.jpeg" alt="Avatar" width={100} height={100} />
+        <p>
+          <span>Шелдон Планктон</span> Бизнесмен
+        </p>
+        <p>
+          Не понимаю почему это считают вкусным. На мой взгляд переоценено.
+        </p>
       </div>
     </>
   );
 };
-
-export async function getServerSideProps(context) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/comments`);
-  const data = await response.json();
-
-  return {
-    props: {
-      reviews: data.slice(0, 20),
-    },
-  };
-}
 
 export default ReviewsPage;
