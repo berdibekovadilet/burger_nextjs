e22ce77b5478c1152ef81815b9b8a81d504bbbe4
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-07-04 23:09:30 +0600

    added burgers page

--- pages/burgers/index.jsx
+++ pages/burgers/index.jsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../../styles/Burger.module.css";
+
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:5000/items");
   const data = await res.json();
@@ -7,11 +11,32 @@ export const getStaticProps = async () => {
   };
 };
 
-const Burgers = (props) => {
-  console.log(props);
+const Burgers = ({ burgers }) => {
   return (
     <div>
       <h1>Наши бургеры</h1>
+      {burgers.map((burger) => {
+        return (
+          <Link href={`/burgers/${burger.id}`} key={burger.id}>
+            <a className={styles.burgerCard}>
+              <div className={styles.imageContainer}>
+                <Image
+                  src={`${burger.image}`}
+                  alt={`${burger.name}`}
+                  width="100%"
+                  height="120%"
+                  layout="responsive"
+                  objectFit="fill"
+                />
+              </div>
+              <div>
+                <h3>{burger.name}</h3>
+                <p>{burger.desc}</p>
+              </div>
+            </a>
+          </Link>
+        );
+      })}
     </div>
   );
 };
